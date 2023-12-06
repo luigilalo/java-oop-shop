4ab49476b89a5f1d09b95aa6ad429727d7d82045
luigilalo <luigilaloe1@gmail.com>
2023-12-06 18:19:59 +0100

    To Improve

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,2 +1,19 @@
-package org.lessons.java.shop;public class Main {
+package org.lessons.java.shop;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Qual è il nome del prodotto?");
+        String productName = scanner.next();
+        System.out.println("Qual è la descrizione del prodotto?");
+        String productDescription = scanner.next();
+        System.out.println("Qual è il prezzo del prodotto?");
+        double price = scanner.nextDouble();
+        System.out.println("Quant'è l'IVA del prodotto?");
+        double vat = scanner.nextDouble();
+        Prodotto product = new Prodotto(productName, productDescription, price, vat);
+        System.out.println(product.infoAndCode());
+    }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,2 +1,57 @@
-package org.lessons.java.shop;public class Prodotto {
+package org.lessons.java.shop;
+
+import java.util.Random;
+
+public class Prodotto {
+
+    // ATTRIBUTI
+    private String productName;
+    private String productDescription;
+    private double price;
+    private double vat;
+
+    // COSTRUTTORI
+    Prodotto(String productName, String productDescription, double price, double vat) {
+        this.productName = productName;
+        this.productDescription = productDescription;
+        this.price = price;
+        this.vat = vat;
+    }
+
+
+
+    // GETTERS & SETTERS
+
+    public int getCode() {
+        return randomCode.nextInt(1000000,99999999);
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getVat() {
+        return vat;
+    }
+
+    // METODI
+    Random randomCode = new Random();
+    public int codeProduct = randomCode.nextInt(1000000,99999999);
+
+    double priceWithVat() {
+        return price + vat;
+    }
+
+    public String infoAndCode(){
+        return productName + " " + productDescription+"-"+codeProduct + " " + priceWithVat() + "euro";
+    }
+
 }
