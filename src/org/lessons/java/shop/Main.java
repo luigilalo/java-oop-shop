f5e63b5308e84e3793557f5c04a6be6214ad2d36
luigilalo <luigilaloe1@gmail.com>
2023-12-07 16:31:55 +0100

    Added Exceptions (to Improve)

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,17 +1,35 @@
 package org.lessons.java.shop;
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        double price = 0;
+        double vat = 0;
+        boolean correctInfo = false;
         Scanner scanner= new Scanner(System.in);
         System.out.println("Qual è il nome del prodotto?");
         String productName = scanner.next();
         System.out.println("Qual è la descrizione del prodotto?");
         String productDescription = scanner.next();
-        System.out.println("Qual è il prezzo del prodotto?");
-        double price = scanner.nextDouble();
-        double vat = 0.40;
+      while (!correctInfo) {
+          try {
+              System.out.println("Qual è il prezzo del prodotto?");
+              price = scanner.nextDouble();
+              if (price < 0) {
+                  throw new IllegalArgumentException(Double.toString(price));
+              }
+              System.out.println("Qual è l'IVA del prodotto?");
+              vat = scanner.nextDouble();
+              if (vat < 0) {
+                  throw new IllegalArgumentException(Double.toString(price));
+              }
+          } catch (IllegalArgumentException e) {
+              System.out.println("Sia prezzo che IVA devono essere valori positivi, inserisci nuovamente il prezzo :)");
+          }
+            if (price > 0 && vat > 0)
+            {correctInfo = true;
+                                  }
+      }
         Prodotto product = new Prodotto(productName, productDescription, price, vat);
         System.out.println("Il prezzo senza IVA sarà di " + product.getPrice());
         System.out.println("Il prezzo con IVA sarà di" + " " + product.priceWithVat());
